23758986a94566d8d9743f9308a00b6b794f2cf8
Vincent-Férié <107779991+vincentferie@users.noreply.github.com>
2022-07-10 01:26:49 +0000

    Ajout de tâches

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from tasks.models import Collection, Tasks
+from tasks.models import Collection, Task
 
-admin.site.register(Tasks)
+admin.site.register(Task)
 admin.site.register(Collection)
--- tasks/models.py
+++ tasks/models.py
@@ -3,11 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Collection(models.Model):
-    objects = None
     name = models.CharField(max_length=80)
     slug = models.SlugField()
 
+    @classmethod
+    def get_default_collection(cls) -> "Collection":
+        collection, _ = cls.objects.get_or_create(name="Défaut", slug="_defaut")
+        return collection
 
-class Tasks(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
     description = models.CharField(max_length=300)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
